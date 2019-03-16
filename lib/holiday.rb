ecde3f8e01408adac91cbe0fff1e41c4a46c1fde
Charlotte Goldthwaite <chargold201@gmail.com>
2019-03-16 22:01:21 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,7 +59,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, data|
     puts "#{season.capitalize}:"
     data.each do |holiday, supplies|
-      puts "  #{holiday.replace("_", " ")}: #{supplies.join(", ")}"
+      puts "  #{holiday.replace("_", " ").capitalize}: #{supplies.join(", ")}"
     end
   end
 end
